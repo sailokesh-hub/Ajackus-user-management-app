b71acc91f758f6163e5f62f013ad0d001de24f33
Sai Lokesh <sailokeshchirumarri@gmail.com>
2024-11-20 10:47:28 +0530

    ninth commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Ajackus-user-management-app/users">
+    <BrowserRouter basename="/Ajackus-user-management-app/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
